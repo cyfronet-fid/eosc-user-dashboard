a00ff6dcf3dc0dae104cc300df20f2ea5c34175c
Daniel Kryska <daniel.kryska94@gmail.com>
2022-03-02 11:54:20 +0100

    chore: remove user verification on recommendations

--- backend/app/routes/recommended_publications.py
+++ backend/app/routes/recommended_publications.py
@@ -10,7 +10,7 @@ from app.utils.request_validators import valid_limit, valid_offset
 router = APIRouter()
 
 
-@router.get("", dependencies=[Depends(cookie)], )
+@router.get("")
 async def get_recommended_publications(
         limit: Optional[int] = 3,
         offset: Optional[int] = 0,
@@ -20,6 +20,11 @@ async def get_recommended_publications(
     valid_offset(limit, offset)
 
     return [
+               RecommendedPublicationResponse(
+                   label="Evaluation der dadadadad",
+                   publish_date="2004-10-01T05:45:09Z",
+                   tags=["open access", "German"]
+               ),
                RecommendedPublicationResponse(
                    label="Evaluation der dadadadad",
                    publish_date="2004-10-01T05:45:09Z",
