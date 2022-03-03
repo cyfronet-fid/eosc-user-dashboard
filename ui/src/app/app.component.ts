3f525c92bc6e3bc76c7cf30d82d64cd12c5b2a6c
Daniel Kryska <daniel.kryska94@gmail.com>
2022-03-03 14:34:23 +0100

    feat: update styles and add possibility to change view template

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,12 +1,34 @@
 import {Component} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   template: `
     <app-main-header></app-main-header>
     <div class="container-xxl">
+      <div id="ab-view-btn">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          [(ngModel)]="isBView"
+          id="flexCheckDefault"
+          (change)="changeTemplate()"
+        >
+        <label class="form-check-label" for="flexCheckDefault">
+          &nbsp;&nbsp;Show B view template
+        </label>
+      </div>
       <router-outlet></router-outlet>
     </div>
   `
 })
-export class AppComponent {}
+export class AppComponent {
+  isBView = false
+
+  constructor(private _router: Router) {
+  }
+
+  async changeTemplate() {
+    await this._router.navigateByUrl(this.isBView ? "b-view" : "")
+  }
+}
