bb8f514ae51367f17ffaea88d9e9db95ce9c02e1
Daniel Kryska <daniel.kryska94@gmail.com>
2022-07-14 11:46:21 +0200

    feat: add dashboard grid
    - use angular-gridster2 lib
    - configure board sizes and limitations
    - separate grid items
    - restyle grid
    - provide edit mode
    - provide possibility to add new items
    - store grid configuration in separate file

--- ui/apps/ui/src/app/dashboard-page/dashboard-page.module.ts
+++ ui/apps/ui/src/app/dashboard-page/dashboard-page.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardPageComponent } from './dashboard-page.component';
+import {GridsterModule} from "angular-gridster2";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [DashboardPageComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, GridsterModule, FormsModule],
 })
 export class DashboardPageModule {}
