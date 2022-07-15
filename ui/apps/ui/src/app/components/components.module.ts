48e63e0a64aae51bf5396b3a91971ba8650834e4
Daniel Kryska <daniel.kryska94@gmail.com>
2022-07-15 11:37:18 +0200

    refactor: split components, layouts and pages

--- ui/apps/ui/src/app/components/components.module.ts
+++ ui/apps/ui/src/app/components/components.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { LibraryWidgetComponent } from './library-widget.component';
+import {LibraryWidgetsSectionComponent} from "./library-widgets-section.component";
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LibraryWidgetComponent, LibraryWidgetsSectionComponent, LibraryWidgetsSectionComponent],
+  imports: [CommonModule],
+  exports: [LibraryWidgetComponent, LibraryWidgetsSectionComponent, LibraryWidgetsSectionComponent],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
--- ui/apps/ui/src/app/layouts/layouts.module.ts
+++ ui/apps/ui/src/app/layouts/layouts.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainHeaderComponent } from './main-header.component';
+import { LibraryComponent } from './library.component';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { ComponentsModule } from '../components/components.module';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { GridComponent } from './grid.component';
+import {GridsterModule} from "angular-gridster2";
 
 @NgModule({
-  declarations: [MainHeaderComponent],
-  imports: [CommonModule],
-  exports: [MainHeaderComponent],
+  declarations: [MainHeaderComponent, LibraryComponent, GridComponent],
+  imports: [CommonModule, NzDrawerModule, ComponentsModule, NzButtonModule, GridsterModule],
+  exports: [MainHeaderComponent, LibraryComponent, GridComponent],
 })
 export class LayoutsModule {}
