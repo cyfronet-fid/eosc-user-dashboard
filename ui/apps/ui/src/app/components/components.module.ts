ad4661214742537c3d1feb5a313877a263563171
Daniel Kryska <daniel.kryska94@gmail.com>
2022-07-15 15:56:03 +0200

    feat: add grid constraints, add fully working lib

--- ui/apps/ui/src/app/components/components.module.ts
+++ ui/apps/ui/src/app/components/components.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LibraryWidgetComponent } from './library-widget.component';
-import {LibraryWidgetsSectionComponent} from "./library-widgets-section.component";
+import { LibraryWidgetsSectionComponent } from './library-widgets-section.component';
 
 @NgModule({
-  declarations: [LibraryWidgetComponent, LibraryWidgetsSectionComponent, LibraryWidgetsSectionComponent],
+  declarations: [
+    LibraryWidgetComponent,
+    LibraryWidgetsSectionComponent,
+    LibraryWidgetsSectionComponent,
+  ],
   imports: [CommonModule],
-  exports: [LibraryWidgetComponent, LibraryWidgetsSectionComponent, LibraryWidgetsSectionComponent],
+  exports: [
+    LibraryWidgetComponent,
+    LibraryWidgetsSectionComponent,
+    LibraryWidgetsSectionComponent,
+  ],
 })
 export class ComponentsModule {}
