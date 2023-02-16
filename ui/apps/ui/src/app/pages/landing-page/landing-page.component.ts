16c00889ac47295bdf819825f8c7c5f0083abae2
Jarek Zbozień <j.zbozien@gmail.com>
2023-02-16 16:58:45 +0100

    landing page mods

--- ui/apps/ui/src/app/pages/landing-page/landing-page.component.ts
+++ ui/apps/ui/src/app/pages/landing-page/landing-page.component.ts
@@ -7,8 +7,8 @@ import { environment } from '@environment/environment';
     <main>
       <div id="topmap">
         <div class="container">
-          <div class="row">
-            <div class="col-sm">
+          <div class="row stars-bg">
+            <div class="col-sm-8">
               <div class="logo-eosc">
                 <img src="assets/eosc-blue.svg" alt="EOSC" />
               </div>
@@ -21,22 +21,6 @@ import { environment } from '@environment/environment';
                 <button class="btn btn-primary" (click)="login()">Login</button>
               </div>
             </div>
-            <div class="col-sm stars-bg">
-              <div class="features-box">
-                <div class="feature-line">
-                  <div class="dot"></div>
-                  Daily recommendations
-                </div>
-                <div class="feature-line">
-                  <div class="dot"></div>
-                  News, events and community
-                </div>
-                <div class="feature-line last">
-                  <div class="dot"></div>
-                  Project management
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
@@ -44,10 +28,12 @@ import { environment } from '@environment/environment';
       <div id="manage">
         <div class="container">
           <div class="row">
-            <div class="col-sm">
-              <div class="image-placeholder"></div>
+            <div class="col-md-7">
+              <div class="image-placeholder">
+                <img src="assets/dashboard-view.png" alt="Dashboard view" />
+              </div>
             </div>
-            <div class="col-sm">
+            <div class="col-md-5">
               <h2>Manage. Discover. Measure.</h2>
               <ul class="check-list">
                 <li>Get notified about news in your domain</li>
@@ -79,8 +65,7 @@ import { environment } from '@environment/environment';
             <div class="col-sm">
               <div class="pre-title">Over 3339935 research resources</div>
               <h3>
-                Publications,<br />Software, Data Sets,<br />
-                Trainings, Services
+                Publications,<br>Software, Data Sets,<br> Learning Resources,<br> Services
               </h3>
               <form class="search-form">
                 <input
@@ -139,7 +124,7 @@ import { environment } from '@environment/environment';
                   your needs and get support from our experts.
                 </div>
                 <div class="btn btn-sm-landing btn-secondary">
-                  Create Research Project
+                  Create a Research Project
                 </div>
               </a>
             </div>
@@ -147,16 +132,81 @@ import { environment } from '@environment/environment';
         </div>
       </div>
 
-      <div id="support">
+      <div id="eosc-navy">
         <div class="container">
           <div class="row">
-            <div class="col-sm-5">
-              <h3>
-                We can provide you with full support for each research step
-              </h3>
-              <a href="#" class="btn btn-sm-landing btn-secondary"
-                >Contact with our experts</a
-              >
+            <div class="col-md-3 col-sm-12 mb-4">
+              <img src="assets/eosc-logo-mono.svg" alt="EOSC Portal Catalogue and Marketplace" class="eosc-logo-navy"/>
+            </div>
+            <div class="col-md-9 col-sm-12">
+                <h4>We are the European Open Science Cloud (EOSC), an environment for hosting and processing research
+                  data to support EU open science</h4>
+            </div>
+          </div>
+
+          <div class="row mt-5">
+            <div class="col-md-3 col-sm-12">
+                <a href="https://marketplace.eosc-portal.eu/" class="eosc-info">
+                  <div class="eosc-info-icon">
+                    <img src="assets/browse-marketplace.svg" alt="Browse Marketplace"/>
+                  </div>
+                  <div class="eosc-info-texts">
+                    <div class="eosc-info-title">
+                      Browse Marketplace
+                    </div>
+                    <div class="eosc-info-subtitle">
+                      Browse through over 3 million research and innovation tools and services, thousands of datasets
+                      from a wide range of research domains from renowned European service providers.
+                    </div>
+                  </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-sm-12">
+              <a href="#" class="eosc-info">
+                <div class="eosc-info-icon">
+                  <img src="assets/dashboard.svg" alt="dashboard"/>
+                </div>
+                <div class="eosc-info-texts">
+                  <div class="eosc-info-title">
+                    My EOSC Dashboard
+                  </div>
+                  <div class="eosc-info-subtitle">
+                    Manage your research projects, access your activity history, and get resource recommendations.
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div class="col-md-3 col-sm-12">
+              <a href="https://eosc-portal.eu/" class="eosc-info">
+                <div class="eosc-info-icon">
+                  <img src="assets/portal.svg" alt="EOSC Portal"/>
+                </div>
+                <div class="eosc-info-texts">
+                  <div class="eosc-info-title">
+                    EOSC Portal
+                  </div>
+                  <div class="eosc-info-subtitle">
+                    Stay up to date with news and events. Read use cases or success stories that highlight how EOSC
+                    resources can support the daily work of researchers and innovators.
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div class="col-md-3 col-sm-12">
+              <a href="https://providers.eosc-portal.eu/home" class="eosc-info">
+                <div class="eosc-info-icon">
+                  <img src="assets/hub.svg" alt="Providers Hub"/>
+                </div>
+                <div class="eosc-info-texts">
+                  <div class="eosc-info-title">
+                    Providers Hub
+                  </div>
+                  <div class="eosc-info-subtitle">
+                    A single platform for providers to onboard their organization into the EOSC portal,
+                    register and manage their resources and gain rich insights about the usage of their offerings.
+                  </div>
+                </div>
+              </a>
             </div>
           </div>
         </div>
@@ -167,34 +217,34 @@ import { environment } from '@environment/environment';
           <div class="row">
             <h3>Stay up to date with what is current in your research field</h3>
             <div class="cards-wrapper">
-              <a href="#" class="card">
+              <a href="https://search.eosc-portal.eu/search/all?q=*&fq=fos:(%2203%20medical%20and%20health%20sciences%22)" class="card">
                 <img src="assets/ico-molecular.svg" alt="Medical" />
                 Medical<br />
                 & Health Sciences
               </a>
-              <a href="#" class="card">
+              <a href="https://search.eosc-portal.eu/search/all?q=*&fq=fos:(%2202%20engineering%20and%20technology%22)" class="card">
                 <img src="assets/ico-technology.svg" alt="Engineering" />
                 Engineering<br />
                 & Technology
               </a>
-              <a href="#" class="card natural-sciences">
+              <a href="https://search.eosc-portal.eu/search/all?q=*&fq=fos:(%2201%20natural%20sciences%22)" class="card natural-sciences">
                 <img src="assets/ico-microscope.svg" alt="Natural Sciences" />
                 Natural Sciences
               </a>
             </div>
             <div class="cards-wrapper">
-              <a href="#" class="card">
+              <a href="https://search.eosc-portal.eu/search/all?q=*&fq=fos:(%2206%20humanities%20and%20the%20arts%22)" class="card">
                 <img
                   src="assets/ico-artificial-intelligence.svg"
                   alt="Humanities"
                 />
                 Humanities
               </a>
-              <a href="#" class="card">
+              <a href="https://search.eosc-portal.eu/search/all?q=*&fq=fos:(%2204%20agricultural%20and%20veterinary%20sciences%22)" class="card">
                 <img src="assets/ico-agriculture.svg" alt="Agricultural" />
                 Agricultural<br />Sciences
               </a>
-              <a href="#" class="card">
+              <a href="https://search.eosc-portal.eu/search/all?q=*&fq=fos:(%2205%20social%20sciences%22)" class="card">
                 <img
                   src="assets/ico-social-science.svg"
                   alt="Social Sciences"
